b13a8eac1aa9e9b261d5e07507bb10e1b15afb0e
Aristides Gutierrez <aristides.gutierrez@gmail.com>
2025-03-14 11:09:15 -0400

    se simplifica el archivo insertar texto y se eliminando archivos desactualizados

--- insertar_textos.py
+++ insertar_textos.py
@@ -1,3 +1,37 @@
+import fitz  # PyMuPDF
+
+letra = "J"
+num1 = "31552665"
+num2 = "7"
+rif = f"{letra}-{num1}-{num2}"
+
+def insertar_varios_textos(input_pdf, output_pdf, textos):
+    doc = fitz.open(input_pdf)
+    
+    for config in textos:
+        pagina = doc[config["pagina"]]
+        
+        # Configuración básica
+        texto = config["texto"]
+        x = config["x"]
+        y = config["y"]
+        font_size = config.get("font_size", 12)
+        font_name = config.get("font_name", "helv")
+        color_text = config.get("color_text", (0, 0, 0))
+
+        # Insertar texto directamente en la posición especificada
+        pagina.insert_text(
+            point=(x, y),
+            text=texto,
+            fontname=font_name,
+            fontsize=font_size,
+            color=color_text
+        )
+    
+    doc.save(output_pdf)
+    doc.close()
+
+# Configuración simplificada
 textos = [
 # REGISTRO DE INFORMACION FISCAL
     # RIF
@@ -146,5 +180,10 @@ textos = [
         "x": 369.30,  
         "y": 561.50, 
     },
-
 ]
+
+insertar_varios_textos(
+    input_pdf="static/Template_Factura_Vertical_SVG.pdf", 
+    output_pdf="new_document/documento_con_texto.pdf",
+    textos=textos
+)
